bc7993862f010a74da4f31f4ca5980d83bfe9675
Joris Lambrechts <joris.lambrechts@kuleuven.be>
2025-11-03 14:24:53 +0100

    add icon and svg for button

--- src/app/export-sp/export-sp.component.ts
+++ src/app/export-sp/export-sp.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'custom-export-sp',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './export-sp.component.html',
   styleUrl: './export-sp.component.scss'
 })
